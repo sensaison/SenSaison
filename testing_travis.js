b307cb304b3bf6c9c8c39af32bdf523ef4b1477c
sapreut <stefan_apr@hotmail.com>
2019-03-27 21:10:04 -0700

    Test

--- testing_travis.js
+++ testing_travis.js
@@ -1,5 +1,5 @@
 var num_1 =  parseInt(prompt('Give me a number!'))
-var num_2 = parseInt(prompt("Give me another number!"))
+var num_2 = parseInt(prompt("Give me another number!"));
 var result;
 
 var operation = prompt('What would you like to do? (add, subtract, multiply, divide)').toUpperCase();
