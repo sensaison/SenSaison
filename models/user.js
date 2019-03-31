a562701c7e263ec45441f038949f2d00c551578c
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-03-31 15:22:36 -0700

    server, config, schema and dummy seeds, api routes

--- models/user.js
+++ models/user.js
@@ -32,7 +32,7 @@ module.exports = function(sequelize, DataTypes) {
     });
 
     User.associate = function(models) {
-        User.hasMany(models.Observation)
+        User.hasMany(models.Observation);
     };
     
     return User;
