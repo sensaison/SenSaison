87c3255a5fabd81ed0bfafffba0a3b4a9b9e480b
sapreut <stefan_apr@hotmail.com>
2019-04-01 20:18:33 -0700

    Further Travis testing and basic map stuff

--- models/observation.js
+++ models/observation.js
@@ -1,71 +1,72 @@
+/* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
-    var Observation = sequelize.define("Observation", {
-        id: {
-            primaryKey: true,
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        user: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        picture_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        time_stamp: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        date_time_obs: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        lat_lon: {
-            type: DataTypes.GEOMETRY,
-            allowNull: true
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        species: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        species_sci_name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        species_confidence: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        weather: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        land_water: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        first_confidence: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        notes: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
+  var Observation = sequelize.define("Observation", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    picture_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    time_stamp: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    date_time_obs: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    lat_lon: {
+      type: DataTypes.GEOMETRY,
+      allowNull: true
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    species_sci_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    species_confidence: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    weather: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    land_water: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    first_confidence: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  Observation.associate = function(models) {
+    Observation.belongsTo(models.User, {
+      foreignkey: {
+        allowNull: false
+      }
     });
+  };
 
-    Observation.associate = function(models) {
-        Observation.belongsTo(models.User, {
-            foreignkey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Observation;
+  return Observation;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,47 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
-    app.get("/api/observations", function(req, res) {
-        db.Observation.findAll({}).then(
-            function(allobs) {
-              res.json(allobs);
-            }
-        );
+  app.get("/api/observations", function(req, res) {
+    db.Observation.findAll({}).then(function(allobs) {
+      res.json(allobs);
     });
+  });
 
-    app.get("/api/users", function(req, res) {
-        db.User.findAll({}).then(
-            function(allusr) {
-              res.json(allusr);
-            }
-        );
+  app.get("/api/users", function(req, res) {
+    db.User.findAll({}).then(function(allusr) {
+      res.json(allusr);
     });
+  });
 
-    app.post("/api/observations", function(req, res) {
-        db.Observation.create(req.body).then(
-            function(newobs) {
-                res.json(newobs);
-            }
-        );
+  app.post("/api/observations", function(req, res) {
+    db.Observation.create(req.body).then(function(newobs) {
+      res.json(newobs);
     });
+  });
 
-    app.post("/api/users", function(req, res) {
-        db.User.create(req.body).then(
-            function(newusr) {
-                res.json(newusr);
-            }
-        );
+  app.post("/api/users", function(req, res) {
+    db.User.create(req.body).then(function(newusr) {
+      res.json(newusr);
     });
+  });
 
-    app.set("/api/observations/:id", function(req, res) {
-        db.Observation.update({
-            where: {
-                id: req.params.id
-            }
-        }).then(
-            function(updobs) {
-                res.json(updobs);
-            }
-        );
-    })
-  
-    app.set("/api/users/:id", function(req, res) {
-        db.User.update({
-            where: {
-                id: req.params.id
-            }
-        }).then(
-            function(updusr) {
-                res.json(updusr);
-            }
-        );
+  app.set("/api/observations/:id", function(req, res) {
+    db.Observation.update({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(updobs) {
+      res.json(updobs);
     });
+  });
+
+  app.set("/api/users/:id", function(req, res) {
+    db.User.update({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(updusr) {
+      res.json(updusr);
+    });
+  });
 };
