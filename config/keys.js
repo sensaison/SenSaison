218cc16a9aaf5c272846cd47a50c29b3e3319bf6
Auri <aurihrp@hotmail.com>
2019-04-06 10:31:35 -0700

    OAuth returning data

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,10 @@
 module.exports = {
   google: {
     clientID:
-      "785370225233-u0ck8j0latbaodgt4h7s0g60cvhmkj7v.apps.googleusercontent.com",
-    clientSecret: "tvzeX9utKOjgU2Q3LfZ9zNI-"
-  }
+      "785370225233-mc4i390arqv9e4pilq7vu4tn7ncstl2n.apps.googleusercontent.com",
+    clientSecret: "7LMArbMUbJLOhQ6iUOcOg-a7"
+  },
+  passwords: {
+    sql: 'Youreprettygood1!'
+}
 };
