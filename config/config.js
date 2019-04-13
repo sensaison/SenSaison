7a445b3a711ebf6a8096fcdd2426a261a02587c1
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-04-13 09:11:18 -0700

    heroku

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ module.exports = {
     // eslint-disable-next-line camelcase
         // use_env_variable: "mysql://ynm4ek0h6e6vnt7l:nylkyc2cq9fsns21@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r6mgqw5hbumksrrd",
         username: process.env.JAWSDB_USER,
-        password: process.env.JAWSDB_PW,
+        password: process.env.JAWSDB_PWD,
         database: process.env.JAWSDB_DB,
         host: process.env.JAWSDB_HOST,
         dialect: "mysql"
