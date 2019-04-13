6f67df7150366be2acc6bafff6e980fcd240ef79
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-04-13 08:56:46 -0700

    heroku

--- config/config.js
+++ config/config.js
@@ -17,7 +17,11 @@ module.exports = {
     },
     production: {
     // eslint-disable-next-line camelcase
-        use_env_variable: "mysql://ynm4ek0h6e6vnt7l:nylkyc2cq9fsns21@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r6mgqw5hbumksrrd",
+        // use_env_variable: "mysql://ynm4ek0h6e6vnt7l:nylkyc2cq9fsns21@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r6mgqw5hbumksrrd",
+        username: "ynm4ek0h6e6vnt7l",
+        password: "nylkyc2cq9fsns21",
+        database: "r6mgqw5hbumksrrd",
+        host: "jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         dialect: "mysql"
     }
     
