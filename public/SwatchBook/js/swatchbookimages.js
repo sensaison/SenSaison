453c81b4a710aa23558ffe5c5fef3a0ad2982248
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-07-09 22:51:30 -0700

    swatchbook api

--- public/SwatchBook/js/swatchbookimages.js
+++ public/SwatchBook/js/swatchbookimages.js
@@ -1,20 +1,54 @@
 $.get("/api/animals/mostrecentone", data => {
-	console.log(data);
-	$("#animals").css("background-image", "url(" + data.animalImage + ")");
+	let imgSrc;
+	if (data.pictureId) {
+		imgSrc = "https://res.cloudinary.com/sensaison/image/upload/"+ data.pictureId + ".jpg";
+	} else {
+		imgSrc = data.animalImage;
+	}
+    $("#sb-animals").attr("src", imgSrc);
+    $("#sb-animals-date").html("Date:", data.dateObs);
 });
     
 $.get("/api/fungus/mostrecentone", data => {
-	$("#fungus").css("background-image", "url(" + data.fungusImage + ")");
+	let imgSrc;
+	if (data.pictureId) {
+		imgSrc = "https://res.cloudinary.com/sensaison/image/upload/"+ data.pictureId + ".jpg";
+	} else {
+		imgSrc = data.fungusImage;
+    }	
+    $("#sb-fungus").attr("src", imgSrc);
+    $("#sb-fungus-date").html("Date:", data.dateObs);
 });
     
 $.get("/api/land_water/mostrecentone", data => {
-	$("#landwater").css("background-image", "url(" + data.landwaterImage + ")");
+	let imgSrc;
+	if (data.pictureId) {
+		imgSrc = "https://res.cloudinary.com/sensaison/image/upload/"+ data.pictureId + ".jpg";
+	} else {
+		imgSrc = data.landwaterImage;
+    }	
+    $("#sb-land-water").attr("src", imgSrc);
+    $("#sb-land-water-date").html("Date:", data.dateObs);
 });
     
 $.get("/api/plants/mostrecentone", data => {
-	$("#plants").css("background-image", "url(" + data.plantsImage + ")");
-});
+	let imgSrc;
+	if (data.pictureId) {
+		imgSrc = "https://res.cloudinary.com/sensaison/image/upload/"+ data.pictureId + ".jpg";
+	} else {
+        imgSrc = data.plantsImage;
+    }
+    $("#sb-plants").attr("src", imgSrc);
+    $("#sb-plants-date").html("Date:", data.dateObs);
+};
     
 $.get("/api/weather/mostrecentone", data => {
-	$("#weather").css("background-image", "url(" + data.weatherImage + ")");
+	let imgSrc;
+	if (data.pictureId) {
+		imgSrc = "https://res.cloudinary.com/sensaison/image/upload/"+ data.pictureId + ".jpg";
+	} else {
+        imgSrc = data.weatherImage;
+    }
+    $("#sb-weather").attr("src", imgSrc);
+    $("#sb-weather-date").html("Date:", data.dateObs);
 });
