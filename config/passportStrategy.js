cef0f1266cf2837225d1c2d7252f6200cc9ef632
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-07-13 19:40:28 -0700

    proxy

--- config/passportStrategy.js
+++ config/passportStrategy.js
@@ -10,10 +10,10 @@ const Passport = require("passport"),
 Passport.use(new GoogleStrategy({
 	clientID: process.env.GOOGLE_CLIENTID,
 	clientSecret: process.env.GOOGLE_SECRET,
-	callbackURL: "/auth/google/callback"
-	// response_type: "code token id_token",
-	// prompt: "select_account",
-	// scope: "openid profile email"
+	callbackURL: "/auth/google/callback",
+	response_type: "code token id_token",
+	prompt: "select_account",
+	scope: "openid profile email"
 },
 (accessToken, refreshToken, profile, done) => {
 
